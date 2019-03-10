12ff2cb30611b7e021b3083029f1218613a308cd
ashok <as@mail.com>
2019-03-10 09:45:35 +0530

    nester error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'ang7app';
+  userForm: any;
+  
+  constructor(private formBuilder: FormBuilder) {
+      
+    this.userForm = this.formBuilder.group({
+      'name': ['', Validators.required],
+      'email': ['', [Validators.required, ValidationService.emailValidator]],
+      'profile': ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+  
+  saveUser() {
+    if (this.userForm.dirty && this.userForm.valid) {
+      alert(`Name: ${this.userForm.value.name} Email: ${this.userForm.value.email}`);
+    }
+  }
 }
